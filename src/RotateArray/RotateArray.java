fa1cec389afc02b816914949340917ad83ed1f50
Suzanne Richman <suzanne@favoredfortune.com>
2018-11-30 08:48:17 -0800

    removing student assigment code and test that I gave for extra work to bored student

--- src/RotateArray/RotateArray.java
+++ src/RotateArray/RotateArray.java
@@ -5,43 +5,43 @@ import java.util.Arrays;
 
 public class RotateArray {
 
-    public static int[] rotateArrayRight (int[] arr, int num) {
-        if (num > 0) {
-            int[] tempArray = arr.clone();
-            for (int i = 0; i < arr.length; i++) {
-                tempArray[i] = i - 1 < 0 ? arr[arr.length - 1] : arr[i + 1];
-            }
-            return rotateArrayRight(tempArray, num - 1);
-        } else {
-            return arr;
-        }
-    }
-
-//    public static int[] rotate(int[] nums, int k) {
-//        if (nums.length < k) {
-//            System.out.println("Input array not long enough to rotate " + k + " spaces. Original array returned. " + Arrays.toString(nums));
-//            return nums;
-//        }
-//        int[] rotated = new int[nums.length];
-//        int extraCounter = 0;
-//        for(int j = k; j< nums.length; j++){
-//            rotated[j]= nums[extraCounter];
-//            System.out.println(Arrays.toString(rotated));
-//            extraCounter++;
-//        }
-//        if(k == nums.length -1) {
-//            for (int i = 0; i < k + 1; i++) {
-//                rotated[i] = nums[nums.length - k];
-//                k--;
-//                System.out.println(Arrays.toString(rotated));
+//    public static int[] rotateArrayRight (int[] arr, int num) {
+//        if (num > 0) {
+//            int[] tempArray = arr.clone();
+//            for (int i = 0; i < arr.length; i++) {
+//                tempArray[i] = i - 1 < 0 ? arr[arr.length - 1] : arr[i + 1];
 //            }
+//            return rotateArrayRight(tempArray, num - 1);
 //        } else {
-//            for (int i = 0; i <= k + 1; i++) {
-//                rotated[i] = nums[nums.length - k];
-//                k--;
-//                System.out.println(Arrays.toString(rotated));
-//            }
+//            return arr;
 //        }
-//        return rotated;
 //    }
+
+    public static int[] rotate(int[] nums, int k) {
+        if (nums.length < k) {
+            System.out.println("Input array not long enough to rotate " + k + " spaces. Original array returned. " + Arrays.toString(nums));
+            return nums;
+        }
+        int[] rotated = new int[nums.length];
+        int extraCounter = 0;
+        for(int j = k; j< nums.length; j++){
+            rotated[j]= nums[extraCounter];
+            System.out.println(Arrays.toString(rotated));
+            extraCounter++;
+        }
+        if(k == nums.length -1) {
+            for (int i = 0; i < k + 1; i++) {
+                rotated[i] = nums[nums.length - k];
+                k--;
+                System.out.println(Arrays.toString(rotated));
+            }
+        } else {
+            for (int i = 0; i <= k + 1; i++) {
+                rotated[i] = nums[nums.length - k];
+                k--;
+                System.out.println(Arrays.toString(rotated));
+            }
+        }
+        return rotated;
+    }
 }
--- test/RotateArray/RotateArrayTest.java
+++ test/RotateArray/RotateArrayTest.java
@@ -10,36 +10,36 @@ public class RotateArrayTest {
     public void rotateTestOne() {
         int[] input = {1,2,3,4,5,6,7};
         int[] expected = {5,6,7,1,2,3,4};
-        int[] actual = RotateArray.rotateArrayRight(input,3);
+        int[] actual = RotateArray.rotate(input,3);
 
         assertArrayEquals(expected, actual);
     }
 
-//    @Test
-//    public void rotateTestTwo() {
-//        int[] input = {-1,-100,3,99};
-//        int[] expected = {3,99,-1,-100};
-//        int[] actual = RotateArray.rotate(input,2);
-//
-//        assertArrayEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void rotateTestNoChange() {
-//        int[] input = {-1,-100};
-//        int[] expected = {-1,-100};
-//        int[] actual = RotateArray.rotate(input,7);
-//
-//        assertArrayEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void rotateTestTiny() {
-//        int[] input = {1,2};
-//        int[] expected = {2,1};
-//        int[] actual = RotateArray.rotate(input,1);
-//
-//        assertArrayEquals(expected, actual);
-//    }
+    @Test
+    public void rotateTestTwo() {
+        int[] input = {-1,-100,3,99};
+        int[] expected = {3,99,-1,-100};
+        int[] actual = RotateArray.rotate(input,2);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void rotateTestNoChange() {
+        int[] input = {-1,-100};
+        int[] expected = {-1,-100};
+        int[] actual = RotateArray.rotate(input,7);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void rotateTestTiny() {
+        int[] input = {1,2};
+        int[] expected = {2,1};
+        int[] actual = RotateArray.rotate(input,1);
+
+        assertArrayEquals(expected, actual);
+    }
 
 }
